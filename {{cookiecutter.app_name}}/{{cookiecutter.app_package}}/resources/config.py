b7a03645f3f39132e4a6a3e8d776392cf39b7934
Mirek Simek <miroslav.simek@gmail.com>
2022-12-01 14:26:16 +0100

    url_prefix

--- {{cookiecutter.app_name}}/{{cookiecutter.app_package}}/resources/config.py
+++ {{cookiecutter.app_name}}/{{cookiecutter.app_package}}/resources/config.py
@@ -4,3 +4,4 @@ class {{cookiecutter.resource_config}}(RecordsUIResourceConfig):
     template_folder = "../theme/templates"
     detail_template = "{{cookiecutter.app_package}}/detail.html"
     search_template = "{{cookiecutter.app_package}}/search.html"
+    url_prefix = "{{cookiecutter.url_prefix}}"
