9915b0896def92ba9a65bd593437c8435fd1aab7
Mirek Simek <miroslav.simek@gmail.com>
2023-03-02 20:27:16 +0100

    ui app

--- {{cookiecutter.app_name}}/{{cookiecutter.app_package}}/resources/config.py
+++ {{cookiecutter.app_name}}/{{cookiecutter.app_package}}/resources/config.py
@@ -1,10 +1,16 @@
 from oarepo_ui.resources.config import RecordsUIResourceConfig
 
 class {{cookiecutter.resource_config}}(RecordsUIResourceConfig):
-    template_folder = "../theme/templates"
-    detail_template = "{{cookiecutter.app_package}}/detail.html"
-    search_template = "{{cookiecutter.app_package}}/search.html"
+    template_folder = "../templates"
     url_prefix = "{{cookiecutter.url_prefix}}"
     blueprint_name = "{{cookiecutter.app_name}}"
     ui_serializer_class = "{{cookiecutter.ui_serializer_class}}"
     api_service = "{{cookiecutter.api_service}}"
+
+    layouts = {
+        "detail": {
+            "layout": "oarepo_ui/generic_detail.html.jinja2",
+            "blocks": {"record_main_content": "{{cookiecutter.app_package}}/main.html"},
+        },
+        "search": {"layout": "oarepo_ui/search.html.jinja2"},
+    }
